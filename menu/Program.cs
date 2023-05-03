389ac2c0175af2a9e10e1bfeee4254785d65a10e
unknown <sesnadia03@gmail.com>
2023-05-03 23:05:10 +0700

    master

---
+++ menu/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, choice;
+            Console.WriteLine("Введите числа А и В:");
+            A = Convert.ToInt32(Console.ReadLine());
+            B = Convert.ToInt32(Console.ReadLine());
+            choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 1, чтобы сложить, 2 - отнять, 3 - умножить  4 - разделить");
+
+            switch (choice)
+            {
+                case '1':
+                    
+                    break;
+                case '2':
+                    
+                    break;
+                case '3':
+                    
+                    break;
+
+                case '4':
+                    
+                    break;
+            }
+
+        }
+    }
+}
